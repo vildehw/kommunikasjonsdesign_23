68d5064b255bfdf5a7ab84d17b808bb86710d8fa
vildehw <vildehw@hiof.no>
2023-11-24 00:19:32 +0100

    oppg4

--- oppg4/index.js
+++ oppg4/index.js
@@ -1,4 +1,7 @@
-  //kilde brukt til pauseknapp https://www.w3schools.com/tags/av_met_pause.asp
+  
+  
+  //pauseknapp 
+  //kilde brukt https://www.w3schools.com/tags/av_met_pause.asp
   function pauseVid(videoId, btnId) {  
     const video = document.getElementById(videoId);  
     const videoBtn =  document.getElementById(btnId);
@@ -9,74 +12,72 @@
       video.pause();
       videoBtn.innerHTML = "Start";
     }
-  }  
+  }   
+
+
+  //Funkjson for å vise boks ved trykk på knapp 
+  function visBoks() { 
+    const faktabutton = document.getElementById("timelineBtn");
+    const timeline = document.getElementById("timeline");
+    if (timeline.style.display === "block") {
+      timeline.style.display = "none";
+      faktabutton.innerHTML = "Klikk for tidslinje over noe"; 
+    } else {
+      timeline.style.display = "block";
+      faktabutton.innerHTML = "Lukk tidslinje over noe";
+    }
+    }
   
 
   //INITIATE MAP
-    const centerLat = 59.2523923
-    const centerLong = 10.7986503
-    //let map = L.map('map').setView([centerLat, centerLong], 13);
-   /* let map = L.map('mapContainer', {
-        center: [centerLat, centerLong],
-        zoom: 10,
-        scrollWheelZoom: false,
-        interactive: true 
-   
-    })*/
-    //SET MAP CONSTRUCTOR
-    /*L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}', {
-        attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
-    }).addTo(map) */
-/*
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { 
-
-        maxZoom: 46, minZoom: 7,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-
-  */  
   let map = L.map('mapContainer', {
     minZoom: 0,
     maxZoom: 5,
     center: [-100, 180],
     zoom: 2,
     crs: L.CRS.Simple, 
-    scrollWheelZoom: false
+    scrollWheelZoom: false,
   });
 
+  //kilde for kode til bruk av bilde i leaflet https://stackoverflow.com/questions/53308706/leafletjs-custom-map-with-a-single-image-png-file-showing-multiple-maps
     const w = 5692;
     const h = 3200;
-    const url = 'elingaardmap.png';
+    const url = 'img/elingaardmap.png';
     const southWest = map.unproject([ 0, h], map.getMaxZoom()-1);
     const northEast = map.unproject([ w, 0], map.getMaxZoom()-1);
     const bounds = new L.LatLngBounds( southWest, northEast);
 
-    L.imageOverlay( url, bounds).addTo(map);
+    L.imageOverlay( url, bounds, {
+      attribution: '<a href="https://freeicons.io/web-application-v.1-3/pin-web-app-gps-location-marker-icon-104645">Ikon: freeicons</a>'
+    }).addTo(map);
 
     map.setMaxBounds(bounds);
 
-/*
-    var imageUrl = 'map2.png',
-   imageBounds = [[59.25391379679315, 10.797237818503786], [59.254396, 10.797226]];
-    L.imageOverlay(imageUrl).addTo(map);
+  //kilde for resterende kode for kart er hentet fra forelesningsmateriell
+  const places = [
+    {name: "-50 50", lat: -87, long: 230, description: "2016"},
+    {name: "0 0", lat: -93, long: 154, description: "2016"},
+    {name: "-100 180", lat:-88, long: 147, description: "2016"},
+  ]   
+  console.log(places)
 
-*/   
+  //ikon hentet fra https://freeicons.io/web-application-v.1-3/pin-web-app-gps-location-marker-icon-104645
+  const markerIcon = L.icon({
+    iconUrl: 'img/marker.svg',
+    iconSize: [45,45],
+    iconAnchor: [20,40],
+    popupAnchor: [0, -20]
+  })
+
+    
+    places.map((place) => {
+      L.marker([place.lat, place.long], {icon: markerIcon})
+      .addTo(map)
+      .bindPopup(`${place.name}`) 
+    }) 
 
 
-const places = [
-        {name: "-50 50", year: "2016", lat: -50, long: 50,},
-        {name: "0 0", year: "2016", lat: 0, long: 0,},
-        {name: "-100 180", year: "2016", lat:-100, long: 180,},
-]
-console.log(places)
-
-const markerIcon = L.icon({
-        iconUrl: 'marker.svg',
-        iconSize: [40,40],
-        iconAnchor: [20,40],
-        popupAnchor: [0, -20]
-    })
-
+    /*
     function fly() {
     L.marker[place.lat, place.long].openPopup();
     map.flyTo([place.lat,place.long],4);
@@ -93,37 +94,29 @@ const markerIcon = L.icon({
     })
     
     document.getElementById("btnContainer").innerHTML = navHTML 
+    */ 
 
 
-function visBoks() { 
-const faktabutton = document.getElementById("timelineBtn");
-const faktaboks = document.getElementById("faktaboks");
-if (faktaboks.style.display === "none" || faktaboks.style.display === "") {
-faktaboks.style.display = "block";
-faktabutton.innerHTML = "Lukk tidslinje over noe";
-} else {
-faktaboks.style.display = "none";
-faktabutton.innerHTML = "Klikk for tidslinje over noe";
-}
-}
+
+  //kode for å bytte bakrgrunn basert på element i viewport er hentet fra forelesningsmateriell.
+  function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }  
 
 
-function isInViewport(el) {
-const rect = el.getBoundingClientRect();
-return (
-rect.top >= 0 &&
-rect.left >= 0 &&
-rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-);
-}  
-
+//scrollheader
 document.addEventListener('scroll', function () {
   if(isInViewport(document.getElementById("htxt1"))) {
-    document.getElementById("changingBg1").style.backgroundImage = "url(elingaardimg.jpg)"
+    document.getElementById("changingBg1").style.backgroundImage = "url(img/elingaardimg.jpg)"
   } 
   if(isInViewport(document.getElementById("htxt2"))) {
-   document.getElementById("changingBg1").style.backgroundImage = "url(IMG_5465.jpg)"
+   document.getElementById("changingBg1").style.backgroundImage = "url(img/IMG_5465.jpg)"
   } 
   }, {
    passive: true
@@ -132,13 +125,13 @@ document.addEventListener('scroll', function () {
 //Scroll Jens 
 document.addEventListener('scroll', function () {
   if(isInViewport(document.getElementById("jtxt1"))) {
-    document.getElementById("changingBg2").style.backgroundImage = "url(closeup.jpg)"
+    document.getElementById("changingBg2").style.backgroundImage = "url(img/closeup.jpg)"
   } 
   if(isInViewport(document.getElementById("jtxt2"))) {
-   document.getElementById("changingBg2").style.backgroundImage = "url(1closeup.jpg)"
+   document.getElementById("changingBg2").style.backgroundImage = "url(img/1closeup.jpg)"
   } 
   if(isInViewport(document.getElementById("jtxt3"))) {
-    document.getElementById("changingBg2").style.backgroundImage = "url(2closeup.jpg)"
+    document.getElementById("changingBg2").style.backgroundImage = "url(img/2closeup.jpg)"
   }
   }, {
    passive: true
@@ -147,26 +140,26 @@ document.addEventListener('scroll', function () {
   //Scroll Birte
 document.addEventListener('scroll', function () {
   if(isInViewport(document.getElementById("btxt1"))) {
-    document.getElementById("changingBg3").style.backgroundImage = "url(closeup.jpg)"
+    document.getElementById("changingBg3").style.backgroundImage = "url(img/closeup.jpg)"
   } 
   if(isInViewport(document.getElementById("btxt2"))) {
-   document.getElementById("changingBg3").style.backgroundImage = "url(1closeup.jpg)"
+   document.getElementById("changingBg3").style.backgroundImage = "url(img/1closeup.jpg)"
   } 
   if(isInViewport(document.getElementById("btxt3"))) {
-    document.getElementById("changingBg3").style.backgroundImage = "url(2closeup.jpg)"
+    document.getElementById("changingBg3").style.backgroundImage = "url(img/2closeup.jpg)"
   }
   }, {
    passive: true
   }); 
 
 
-
+//scrollend
   document.addEventListener('scroll', function () {
 if(isInViewport(document.getElementById("etxt1"))) {
-  document.getElementById("changingBg4").style.backgroundImage = "url(elingaardimg.jpg)"
+  document.getElementById("changingBg4").style.backgroundImage = "url(img/elingaardimg.jpg)"
 } 
 if(isInViewport(document.getElementById("etxt2"))) {
- document.getElementById("changingBg4").style.backgroundImage = "url(IMG_5465.jpg)"
+ document.getElementById("changingBg4").style.backgroundImage = "url(img/IMG_5465.jpg)"
 } 
 }, {
  passive: true
